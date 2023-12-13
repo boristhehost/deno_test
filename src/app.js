a3b2775fbe5b646e778b0a7a0f50d75ebe7a7432
boristhehost <150488101+boristhehost@users.noreply.github.com>
2023-12-13 08:38:16 +0000

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express=require('express')
+
+const app=express()
+const PORT=process.env.PORT||3000
+
+app.get('/hello',(req,res)=>{
+	res.json('hello world')
+})
+
+app.listen(PORT,()=>{
+	console.log('app runing on port ',PORT)
+})
